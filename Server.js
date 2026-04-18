ff049bfaa6f6f91442590f4c6697259c567a7e32
shailkumari <shailykatiyar36@gmail.com>
2026-04-18 17:41:06 +0530

    create server and start

--- Server.js
+++ Server.js
@@ -1,44 +1,16 @@
-/* carete server =>  npm init -y 
-   
-   npm install express
-   node server.js
-   open
-   */
-       
-   //................C R E A T E  S E R V E R .......................     
+const express = require("express");
 
-/* const express = require ('express');
+const app = express();
 
- const app = express();// server create 
+app.get("/",(reuest,response) => {
 
-//  user resquest and response
+    response.send("home");
 
-   app.get('/', (req,res)=>{
+})
 
-      res.send('hello world')
-   })
+app.get('/about',(request,response) => {
 
-   app.get('/about',(req,res) =>{
-    res.send('about page')
-   })
+    response.send("about");
+})
 
- app.listen(3000)  //start server 
-
- */
-
-    //   -------------- R E S T   A P I --------------------
-
-      
-
-      const app = require ('./src/App');
-
-      const connectDB = require('./src/db/db');
-
-      connectDB();
-      // server start 
-
-      app.listen(3000,()=>{
-
-        console.log('server is running at port no 3000');
-
-      })
+app.listen(3000);
